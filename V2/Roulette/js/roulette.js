586a28e3961bed141cf4ec81a884740fca2e897d
Facundo Samartino <facundo.samartino@gmail.com>
2016-06-26 19:18:55 -0300

    Se elimino todo V1, se creo el index.html y se le aplico estilo

--- V2/Roulette/js/roulette.js
+++ V2/Roulette/js/roulette.js
@@ -1,17 +1,40 @@
 "use strict";
 var cantidadNumeros;
-var ruleta={};
+var apuestas={};
 
-function NumerosRuleta(){
-  var i;
-  var j= cantidadNumeros;
-  for( i=0; i<j; i++){
-    ruleta.valor=i[i]
-  }
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 $("#btnllenarRuleta").on("click", function (){
     cantidadNumeros = $("#llenarRuleta").val();
-    NumerosRuleta();
-  }
+    var i = 0;
+    for (i=0; i<cantidadNumeros; i++){
+    	var numero = "<div class='numero-ruleta' numero="+i+">"+i+"</div>";
+		$('.contenedor-ruleta').append(numero);
+    }
+ }
+);
+
+$('.contenedor-ruleta').on('click', '.numero-ruleta', function() {
+	var numero = $(this).attr('numero');
+	console.log(numero);
+	apuestas[numero] = '10';
+ }
+);
+
+$('#ver-json').on('click',function(){
+	console.log(apuestas);
+ }
+);
+
+$('#apostar').on('click',function(){
+	var resultadoRuleta = randomIntFromInterval(0,cantidadNumeros);
+	if ((apuestas[resultadoRuleta] == 0) || (apuestas[resultadoRuleta] == undefined) || (apuestas[resultadoRuleta] == '')){
+		console.log('perdiste');
+	}
+  else{
+		console.log('ganaste');
+	}
+ }
 );
