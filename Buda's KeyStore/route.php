4351d9fc186823823f71460547c7125c6caa1916
SSAAMMAA <Ruben12150325>
2017-06-21 15:00:08 -0300

    Agrego el route para manipular la pagina Closes #30

--- Buda's KeyStore/route.php
+++ Buda's KeyStore/route.php
@@ -1,7 +1,106 @@
 <?php
-require_once('controller_index.php');
+require_once('controllers/controller_index.php');
+require_once('controllers/controller_version.php');
+require_once('controllers/controller_game.php');
+require_once('controllers/controller_form_game.php');
+require_once('controllers/controller_notFound.php');
+require_once('controllers/controller_register.php');
+require_once('controllers/controller_suggestion.php');
+require_once('controllers/controller_listGames.php');
 require_once('config/config_app.php');
+
+function parsearUrl($url){
+  $arrData = explode("/",$url);
+  if (isset($arrData[0])){
+  $arrInfo[configApp::$RESOURCE] = $arrData[0];
+  }
+  if(sizeof($arrData)>1){
+      if (isset($arrData[1])){
+        $arrInfo[configApp::$ACTION] = $arrData[1];
+      }
+      if (isset($arrData[2])){
+        $arrInfo[configApp::$PARAMETERS] = $arrData[2];
+      }
+    }
+  return $arrInfo;
+}
+session_start();
+if (isset($_SESSION['loged'])){
+  $session=TRUE;
+}
+else{
+  $session=FALSE;
+}
+$datos=parsearUrl($_REQUEST[configApp::$ACTION]);
 $controllerIndex=new controller_index();
-$controllerIndex->view_index();
-var_dump($_REQUEST);
+$controllerVersion=new controller_version();
+$controllerGame=new controller_game();
+$controllerFormGame=new controller_form_game();
+$controllerNotFound=new controller_notFound();
+$controllerRegister=new controller_Register();
+$controllerSuggestion=new controller_suggestion();
+$controllerListGames=new controller_listGames();
+if($_GET[configApp::$ACTION] == ''){
+  $controllerIndex->view_index($session);
+}
+else{
+  if ($datos[configApp::$RESOURCE] == 'version'){
+    if ($datos[configApp::$ACTION] == 'formulario'){
+      $controllerVersion->view_version($session);
+    }
+    if ($datos[configApp::$ACTION] == 'insertVersion'){
+      $controllerVersion->insert_version();
+      $controllerVersion->view_version($session);
+    }
+    if ($datos[configApp::$ACTION] == 'modificar'){
+    $controllerVersion->get_modificated_version($datos[configApp::$PARAMETERS],$session);
+    }
+    if ($datos[configApp::$ACTION] == 'modifyVersion'){
+    $controllerVersion->modify_version($datos[configApp::$PARAMETERS],$session);
+    }
+  }
+  if ($datos[configApp::$RESOURCE] == 'juego'){
+    if ($datos[configApp::$ACTION] == 'detalles'){
+      $controllerGame->showDetailedGame($datos[configApp::$PARAMETERS],$session);
+    }
+    if ($datos[configApp::$ACTION] == 'formulario'){
+      $controllerFormGame->view_form_game($session);
+    }
+    if ($datos[configApp::$ACTION] == 'insertGame'){
+      $controllerFormGame->insert_game();
+      $controllerFormGame->view_form_game($session);
+    }
+  }
+  if ($datos[configApp::$RESOURCE] == 'not_found'){
+    $controllerNotFound->view_notFound($session);
+  }
+  if ($datos[configApp::$RESOURCE] == 'login'){
+    if ($datos [configApp::$ACTION] == 'register'){
+      $controllerRegister->view_register($session);
+    }
+    if ($datos [configApp::$ACTION] == 'insertNewUsr'){
+      $controllerRegister->insertNewUsr();
+      $controllerRegister->view_register($session);
+    }
+    if ($datos [configApp::$ACTION] == 'signin'){
+      $controllerRegister->signIn();
+    }
+  }
+  if ($datos [configApp::$RESOURCE] == 'suggestion'){
+    if ($datos [configApp::$ACTION] == 'write'){
+      $controllerSuggestion->view_suggestion($session);
+  }
+    if ($datos [configApp::$ACTION] == 'send'){
+      $controllerSuggestion->send_suggestion();
+      $controllerSuggestion->view_suggestion($session);
+    }
+  }
+  if ($datos [configApp::$RESOURCE] == 'listGames'){
+    $controllerListGames->show_listGames($session);
+}
+  if ($datos [configApp::$RESOURCE] == 'logout'){
+    session_destroy();
+    $controllerIndex->view_index($session);
+  }
+}
 ?>
